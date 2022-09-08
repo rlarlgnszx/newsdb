cd7cb35f06576fce0b18a9843216e31cd265dafb
rlarlgnszx <rlarlgnszx@naver.com>
2022-09-08 09:38:06 +0900

    model

---
+++ download_binary.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7864f2bbdb16487f8548df31b2e7421aa7c2e50a432a2049cf474fd0dd3e3dee
+size 666
---
+++ get_model_binary.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2442702c5595efaa95dbceb99a445d3c3efbee81665b7bc5d0aecc70c21b1330
+size 613
